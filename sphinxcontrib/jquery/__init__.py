d33a817005baf04254f5ed48f79f7eba3644c20d
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-10-26 17:49:05 +0100

    Use local jQuery source
    - Vendor jQuery within the extension
    - Include the Sphinx compatability file
    - Update tests

---
+++ sphinxcontrib/jquery/__init__.py
@@ -0,0 +1,31 @@
+from os import makedirs, path
+import shutil
+
+import sphinx
+
+__version__ = "2.0.0"
+version_info = (2, 0, 0)
+
+_ROOT_DIR = path.abspath(path.dirname(__file__))
+
+
+def setup(app):
+    jquery_installed = getattr(app, "_sphinxcontrib_jquery_installed", False)
+    if sphinx.version_info[:2] >= (6, 0) and not jquery_installed:
+        makedirs(path.join(app.outdir, '_static'))
+        for filename in (
+            'jquery.js',
+            '_sphinx_javascript_frameworks_compat.js',
+        ):
+            app.add_js_file(filename, priority=100)
+            shutil.copyfile(
+                path.join(_ROOT_DIR, filename),
+                path.join(app.outdir, '_static', filename)
+            )
+        app._sphinxcontrib_jquery_installed = True
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+        "version": __version__,
+    }
